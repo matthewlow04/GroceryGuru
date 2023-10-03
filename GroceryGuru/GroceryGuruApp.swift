552563220ab090f6c90e91d13bc3a20f5cf522ed
Matthew Low <matthew7419@gmail.com>
2023-10-03 10:58:31 -0400

    Initial Commit

---
+++ GroceryGuru/GroceryGuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryGuruApp.swift
+//  GroceryGuru
+//
+//  Created by Matthew Low on 2023-10-03.
+//
+
+import SwiftUI
+
+@main
+struct GroceryGuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
